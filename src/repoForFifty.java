3ec218ab8d9d36cf0964b67bde4c0771b0915d98
Yosif Balabanski <ybalabanski4367@gmail.com>
2024-02-18 23:08:12 +0200

    Skreee

--- src/repoForFifty.java
+++ src/repoForFifty.java
@@ -6,15 +6,14 @@ public class repoForFifty {
 
         String stringy = "jjjsuhrjrsjrjarjtkrthjaerth456872yv98tyw398t230-jmtc097  !~ 0T7UY908TYV0WA=TY4=23itquy0ueygi8h9q8ag44362136436jerajjjk";
         Map<Character, Integer> hashMap = new HashMap<>();
-        for(int i = 0;i<127;i++){
-            char c = (char)i;
-            hashMap.put( c, 0);
-        }
+
         //Sets the keys to each ASCII symbol and their values to 0
         for (int i = 0; i < stringy.length(); i++) {
             char c = stringy.charAt(i);
             if(hashMap.containsKey(c)){
                 hashMap.put(c, hashMap.get(c) + 1);
+            }else{
+                hashMap.put(c,1);
             }
         }
         //Prints all the keys which were found inside the string
